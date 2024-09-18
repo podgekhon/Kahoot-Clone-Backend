7711d3bdf00bdfc231dce843dc47415f91645a0b
Guanlin Kong <z5557617@ad.unsw.edu.au>
2024-09-18 17:32:42 +1000

    add function adminUserPasswordUpdate in auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,14 +1,27 @@
 /**
   * <Brief description of what the function does>
   * 
-  * @param {Integers} authUserId - description of paramter
-  * @param {String} email - description of paramter
-  * @param {String} nameFirst - description of paramter
-  * @param {String} nameLast - description of paramter
+  * @param {Integers} authUserId - authUserId
+  * @param {String} email - email
+  * @param {String} nameFirst - First name
+  * @param {String} nameLast - Last name
   * ...
-  * no return;
+  * @return {} no return;
 */
 const adminUserDetailsUpdate = ( authUserId, email, nameFirst, nameLast ) => {
+    return { }
+}
+
+/**
+  * <Brief description of what the function does>
+  * 
+  * @param {Integers} authUserId - description of paramter
+  * @param {String} oldPassword - oldPassword
+  * @param {String} newPassword - newPassword
+  * ...
+  * @return {} no return;
+*/
+const adminUserPasswordUpdate = ( authUserId, oldPassword, newPassword  ) => {
     return { }
 }
 
