58c8a1a9d05421dccbf58b7988c2a352057b0c65
Eric Yang <z5589000@ad.unsw.edu.au>
2024-09-18 17:31:52 +1000

    added description

--- src/quiz.js
+++ src/quiz.js
@@ -1,3 +1,12 @@
+/**
+  * Given basic details about a new quiz, create one for the logged in user.
+  * 
+  * @param {integer} authUserId - description of paramter
+  * @param {integer} quizId - description of parameter
+  * 
+  * @returns {integer} - description of condition for return
+*/
+
 const adminQuizCreate = (authUserId, name, description) => {
     return {
         quizId: 2,
