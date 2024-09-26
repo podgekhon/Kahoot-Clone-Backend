c8b9511603f5f2aa4fcf5ee57291a62761e9ab92
Patrick Tang Truong <z5549057@ad.unsw.edu.au>
2024-09-26 18:07:53 +1000

    feat: add invalid input test cases

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -6,22 +6,27 @@ beforeEach(async () => {
     // await clear();
   });
 
-describe('tests for adminQuizCreate', () => {
+describe('test for adminQuizCreate', () => {
     const tooLongString =   'abcdefghijklmnopqrstuvwxyz' +
                             'abcdefghijklmnopqrstuvwxyz' +
                             'abcdefghijklmnopqrstuvwxyz' +
                             'abcdefghijklmnopqrstuvwxyz';
     
     test.each([
-        {authUserId: -100, name: 'pat', description: tooLongString},
-    ])('authUserId: $authUserId, name: $name, description: tooLongString', ({authUserId, name, description}) => {
-        
+        //these tests will check for invalid inputs
+        {authUserId: -100, name: 'pat1', description: 'science', output: {error: 'AuthUserId is not a valid user.'}},
+        {authUserId: 2, name: 'pat_12', description: 'science', output: {error: 'Name contains invalid characters. Valid characters are alphanumeric and spaces.'}},
+        {authUserId: 2, name: 'pat1', description: 'science', output: {error: 'Name is already used by the current logged in user for another quiz.'}},
+        {authUserId: 2, name: 'pa', description: 'science', output: {error: 'Name is either less than 3 characters long or more than 30 characters long.'}},
+        {authUserId: 2, name: 'abcdefghijklmnopqrstuvwxyz12345', description: 'science', output: {error: 'Name is either less than 3 characters long or more than 30 characters long.'}},
+        {authUserId: 2, name: 'pat', description: tooLongString, output: {error: 'Description is more than 100 characters in length'}},
+
+        //idk about testing for valid inputs, i think we should tho
+        {authUserId: 2, name: 'pat', description: 'science', output: {quizId: 2}},
+
+
+
+    ])(`($authUserId, $name, $description) : $output`, ({authUserId, name, description, output}) => {
+        expect(adminQuizCreate(authUserId, name, description)).toStrictEqual(output)
     })
 })
-
-// describe('testing adminQuizCreate()', () => {
-//     test('Check valid AuthUserId', () => {
-//         const quizCreate = adminQuizCreate(1, 'pat', 'science');
-//         expect(quizCreate).toStrictEqual({quizId: 2});
-//     })
-// })
