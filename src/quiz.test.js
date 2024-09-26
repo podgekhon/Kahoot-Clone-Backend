b7dce2962574b3bffc9b753b6063f8e904ad9adb
Patrick Tang Truong <z5549057@ad.unsw.edu.au>
2024-09-26 21:45:47 +1000

    style: fix test descriptions for adminQuizCreate

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -6,7 +6,7 @@ beforeEach(async () => {
     // await clear();
   });
 
-describe('test for adminQuizCreate', () => {
+describe('adminQuizCreate', () => {
     const tooLongString =   'abcdefghijklmnopqrstuvwxyz' +
                             'abcdefghijklmnopqrstuvwxyz' +
                             'abcdefghijklmnopqrstuvwxyz' +
@@ -14,19 +14,65 @@ describe('test for adminQuizCreate', () => {
     
     test.each([
         //these tests will check for invalid inputs
-        {authUserId: -100, name: 'pat1', description: 'science', output: {error: 'AuthUserId is not a valid user.'}},
-        {authUserId: 2, name: 'pat_12', description: 'science', output: {error: 'Name contains invalid characters. Valid characters are alphanumeric and spaces.'}},
-        {authUserId: 2, name: 'pat1', description: 'science', output: {error: 'Name is already used by the current logged in user for another quiz.'}},
-        {authUserId: 2, name: 'pa', description: 'science', output: {error: 'Name is either less than 3 characters long or more than 30 characters long.'}},
-        {authUserId: 2, name: 'abcdefghijklmnopqrstuvwxyz12345', description: 'science', output: {error: 'Name is either less than 3 characters long or more than 30 characters long.'}},
-        {authUserId: 2, name: 'pat', description: tooLongString, output: {error: 'Description is more than 100 characters in length'}},
+        {
+            //assuming that authUserId cannot be negative??
+            authUserId: -100, name: 'pat1', 
+            description: 'science', 
+            output: {error: 'AuthUserId is not a valid user.'},
+            testDescription: 'invalid authUserId',
+        },
 
-        //idk about testing for valid inputs, i think we should tho
-        {authUserId: 2, name: 'pat', description: 'science', output: {quizId: 2}},
+        {
+            authUserId: 2, 
+            name: 'pat_!@#', 
+            description: 'science', 
+            output: {error: 'Name contains invalid characters. Valid characters are alphanumeric and spaces.'},
+            testDescription: 'name contains invalid characters',
+        },
+        {
+            authUserId: 2, 
+            name: 'pat1', 
+            description: 'science', 
+            output: {error: 'Name is already used by the current logged in user for another quiz.'},
+            testDescription: 'name already used for another quiz owned by same user',
+        },
+        {
+            authUserId: 2, 
+            name: 'pa', 
+            description: 'science', 
+            output: {error: 'Name is either less than 3 characters long or more than 30 characters long.'},
+            testDescription: 'name less than 3 characters',
+        },
+        {
+            authUserId: 2, 
+            name: 'abcdefghijklmnopqrstuvwxyz12345', 
+            description: 'science', 
+            output: {error: 'Name is either less than 3 characters long or more than 30 characters long.'},
+            testDescription: 'name more than 30 characters',
+
+        },
+        {
+            authUserId: 2, 
+            name: 'pat', 
+            description: tooLongString, 
+            output: {error: 'Description is more than 100 characters in length'},
+            testDescription: 'description is more than 100 characters',
+
+        },
+
+        //valid input tests
+        {
+            authUserId: 2, 
+            name: 'pat', 
+            description: 'science', 
+            output: {quizId: 2},
+            testDescription: 'valid inputs',
+
+        },
 
 
 
-    ])(`($authUserId, $name, $description) : $output`, ({authUserId, name, description, output}) => {
+    ])(`$testDescription`, ({authUserId, name, description, output}) => {
         expect(adminQuizCreate(authUserId, name, description)).toStrictEqual(output)
     })
 })
