c590f97e0372e0e2966f93f7a8699f45a84329fc
Patrick Tang Truong <z5549057@ad.unsw.edu.au>
2024-09-26 16:47:35 +1000

    feat: re-add quiz.test.js file

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -18,6 +18,7 @@ describe('tests for adminQuizCreate', () => {
         
     })
 })
+
 // describe('testing adminQuizCreate()', () => {
 //     test('Check valid AuthUserId', () => {
 //         const quizCreate = adminQuizCreate(1, 'pat', 'science');
