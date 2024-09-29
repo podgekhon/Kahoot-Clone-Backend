d7e5c2de155c529fbdb4afa972c93dfff2b754fa
Guanlin Kong <z5557617@ad.unsw.edu.au>
2024-09-29 20:33:55 +1000

    add data in dataStore.js

--- src/dataStore.js
+++ src/dataStore.js
@@ -1,8 +1,85 @@
 // YOU MAY MODIFY THIS OBJECT BELOW
 let data = {
-  users: [],  // This will store all registered users
-  quizzes: [], // This will store all quizzes
+  users: [
+    {
+      authUserId: 1,
+      nameFirst: "Patrick",
+      nameLast: "Truong",
+      email: "pat@gmail.com",
+      numSuccessfulLogins: 3,
+      numFailedPasswordsSinceLastLogin: 1,
+      oldPasswords: ["oldPass123", "olderPass123"],
+      currentPassword: "newPass123",
+    },
+    {
+      authUserId: 2,
+      nameFirst: "John",
+      nameLast: "Doe",
+      email: "john@gmail.com",
+      numSuccessfulLogins: 5,
+      numFailedPasswordsSinceLastLogin: 0,
+      oldPasswords: ["johnOldPass1", "johnOldPass2"],
+      currentPassword: "johnNewPass",
+    },
+  ],
+  quizzes: [
+    {
+      quizId: 1,
+      ownerId: 1,
+      name: "maths",
+      description: "this very hard maths quiz",
+      quiz: [
+        {
+          question: "1 + 1 = ?",
+          answers: [1, 2, 4, 5],
+        },
+        {
+          question: "1 x 1 = ?",
+          answers: [1, 2, 4, 5],
+        },
+      ],
+      timeCreated: 1231343122,
+      timeLastEdited: 132145231415,
+    },
+    {
+      quizId: 2,
+      ownerId: 2,
+      name: "code ",
+      description: "this very hard code quiz",
+      quiz: [
+        {
+          question: "What is H2O?",
+          answers: ["Water", "Oxygen", "Hydrogen", "Nitrogen"],
+        },
+        {
+          question: "What is the speed of light?",
+          answers: ["300,000 km/s", "150,000 km/s", "1,000,000 km/s", "50,000 km/s"],
+        },
+      ],
+      timeCreated: 2341343123,
+      timeLastEdited: 242145231416,
+    },
+    {
+      quizId: 3,
+      ownerId: [1, 2],
+      name: "history",
+      description: "a quiz about world history",
+      quiz: [
+        {
+          question: "Who was the first president of the USA?",
+          answers: ["George Washington", "Abraham Lincoln", "John Adams", "Thomas Jefferson"],
+        },
+        {
+          question: "In what year did World War II end?",
+          answers: [1945, 1941, 1939, 1950],
+        },
+      ],
+      timeCreated: 3451343124,
+      timeLastEdited: 352145231417,
+    },
+  ],
 };
+
 
 // YOU MAY MODIFY THIS OBJECT ABOVE
 
