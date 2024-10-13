3c48324f4dd3eef433cf925907d2c2120ae58f31
Xiaoyuan Ma <z5538765@ad.unsw.edu.au>
2024-10-13 17:01:49 +1100

    added: added test for clear

---
+++ src/tests/clear.test.ts
@@ -0,0 +1,55 @@
+import request from 'sync-request-curl';
+import { url, port } from '../config.json';
+
+const SERVER_URL = `${url}:${port}`;
+
+test('test clear successfule', () => {
+  // let user : { token: string };
+  // let quiz: { quizId: string };
+
+  // register a user
+  const usersession = request('POST', SERVER_URL + '/v1/admin/auth/register', {
+    json: {
+      email: 'EricMa@ad.unsw.edu.au',
+      password: 'EricMa1234',
+      nameFirst: 'Eric',
+      nameLast: 'Ma'
+    },
+    timeout: 100,
+  });
+  const user = JSON.parse(usersession.body as string);
+
+  // create a quiz
+  const quizsession = request('POST', SERVER_URL + '/v1/admin/auth/register', {
+    json: {
+      token: user.token,
+      name: 'My Quiz Name',
+      description: 'A description of my quiz'
+    },
+    timeout: 100,
+  });
+  const quiz = JSON.parse(quizsession.body as string);
+
+  // clear up everything so will return error if a get request is called
+  request('DELETE', SERVER_URL + '/v1/clear', { timeout: 100 });
+  const userListRes = request('GET', SERVER_URL + '/v1/admin/user/details', {
+    qs: {
+      token: user.token,
+    }
+  });
+  expect(JSON.parse(userListRes.body.toString())).toStrictEqual({ error: expect.any(String) });
+
+  const quizListRes = request('GET', SERVER_URL + '/v1/admin/quiz/list', {
+    qs: {
+      token: user.token,
+    }
+  });
+  expect(JSON.parse(quizListRes.body.toString())).toStrictEqual({ error: expect.any(String) });
+
+  const quizRes = request('GET', SERVER_URL + `/v1/admin/quiz/${quiz.quizId}`, {
+    qs: {
+      token: user.token,
+    }
+  });
+  expect(JSON.parse(quizRes.body.toString())).toStrictEqual({ error: expect.any(String) });
+});
