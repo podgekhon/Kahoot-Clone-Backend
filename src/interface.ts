a5e4e916802c0f6dc1db2d81691f4a889def8b70
Patrick Chen <z5592942@ad.unsw.edu.au>
2024-10-18 18:27:22 +1100

    feat: implemented adminQuizQuestionCreate function to handle quiz question creation

--- src/interface.ts
+++ src/interface.ts
@@ -1,52 +1,55 @@
 /// ///////////// interface for dataStore /////////////////
 export interface dataStore {
-    users: user[],
-    quizzes: quiz[],
-    sessions: token[];
-    trash: quiz[];
-  }
+  users: user[],
+  quizzes: quiz[],
+  sessions: token[];
+  trash: quiz[];
+}
 
 export interface user {
-    userId: number,
-    nameFirst: string,
-    nameLast: string,
-    name: string,
-    email: string,
-    numSuccessfulLogins: number,
-    numFailedPasswordsSinceLastLogin: number,
-    oldPasswords: string[],
-    currentPassword: string,
-  }
+  userId: number,
+  nameFirst: string,
+  nameLast: string,
+  name: string,
+  email: string,
+  numSuccessfulLogins: number,
+  numFailedPasswordsSinceLastLogin: number,
+  oldPasswords: string[],
+  currentPassword: string,
+}
 
-export interface quiz {
-    quizId: number,
-    ownerId: number,
-    name: string,
-    description: string,
-    question: object
-    timeCreated: number,
-    timeLastEdited: number,
-  }
-
-export interface token {
-    sessionId: number;
-    userId: number;
-  }
+export interface answerOption {
+  answerId: number;
+  answer: string;
+  colour: string;
+  correct: boolean;
+}
 
 export interface question {
-token: string,
-questionBody: {
-question: string,
-timelimit: number,
-points: number,
-answerOptions: answers[]
-}
+  questionId: number;
+  question: string;
+  timeLimit: number;
+  points: number;
+  answerOptions: answerOption[];
 }
 
-interface answers {
-answer: string,
-correct: boolean
+export interface quiz {
+  quizId: number;
+  ownerId: number;
+  name: string;
+  description: string;
+  numQuestions: number;
+  questions: question[];  
+  timeCreated: number;
+  timeLastEdited: number;
+  timeLimit: number;
 }
+
+export interface token {
+  sessionId: number;
+  userId: number;
+}
+
 /// /////////////// interface for auth.ts/////////////////////
 
 export interface errorMessages {
@@ -79,6 +82,10 @@ export interface quizList {
 
 export interface quizCreateResponse {
   quizId: number,
+}
+
+export interface quizQuestionCreateResponse {
+  questionId: number,
 }
 
 export interface quizInfo {
