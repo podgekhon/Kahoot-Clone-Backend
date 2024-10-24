abe09aca07aa48161ff503999353faa256a0f06c
Xiaoyuan Ma <z5538765@ad.unsw.edu.au>
2024-10-24 16:22:22 +1100

    feat: add wrapper functions for passwordUpdate test

--- src/helperfunctiontests.ts
+++ src/helperfunctiontests.ts
@@ -15,7 +15,7 @@ import { clear } from './other';
 import { adminAuthRegister } from './auth';
 
 // clear
-export const clearHttp = (): {
+export const requestClear = (): {
 statusCode: number,
 body: ReturnType <typeof clear> } => {
   const res = request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });
@@ -38,3 +38,28 @@ body: ReturnType <typeof adminAuthRegister> } => {
   });
   return { body: JSON.parse(res.body.toString()), statusCode: res.statusCode };
 };
+
+
+// adminUserPasswordUpdate
+export const requestAdminUserPasswordUpdate = (
+  token: string,
+  oldPassword: string,
+  newPassword: string
+): {
+  statusCode: number,
+  body: ReturnType <typeof adminAuthRegister>
+} => {
+  const res = request(
+    'PUT',
+    SERVER_URL + '/v1/admin/user/password',
+    {
+      json: {
+        token: token,
+        oldPassword: oldPassword,
+        newPassword: newPassword
+      },
+      timeout: TIMEOUT_MS
+    }
+  );
+  return { body: JSON.parse(res.body.toString()), statusCode: res.statusCode};
+}
