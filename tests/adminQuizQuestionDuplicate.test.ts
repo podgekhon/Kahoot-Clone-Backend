84f0266ad7247bf966c64eea331d2672a99ad6c6
Xiaoyuan Ma <z5538765@ad.unsw.edu.au>
2024-10-24 21:40:36 +1100

    perf: using helper function for questionDuplicate

--- tests/adminQuizQuestionDuplicate.test.ts
+++ tests/adminQuizQuestionDuplicate.test.ts
@@ -1,5 +1,8 @@
 import request from 'sync-request-curl';
 import { port, url } from '../src/config.json';
+import { requestAdminAuthRegister, requestAdminQuizCreate, requestAdminQuizQuestionDuplicate, requestAdminQuizQuestionCreate, requestAdminQuizInfo } from '../src/helperfunctiontests';
+import { quizCreateResponse, quizQuestionCreateResponse, tokenReturn } from '../src/interface';
+import { httpStatus } from './adminAuthRegister.test';
 
 const SERVER_URL = `${url}:${port}`;
 const TIMEOUT_MS = 100 * 1000;
@@ -14,27 +17,11 @@ describe('test for quiz duplicate', () => {
   let question1Id: number;
   // register a user, create a quiz, create a question
   beforeEach(() => {
-    const registerResponse = request('POST', SERVER_URL + '/v1/admin/auth/register', {
-      json: {
-        email: 'admin@unsw.edu.au',
-        password: 'AdminPass1234',
-        nameFirst: 'Admin',
-        nameLast: 'User'
-      },
-      timeout: TIMEOUT_MS
-    });
-    user1token = JSON.parse(registerResponse.body.toString()).token;
+    const user1 = requestAdminAuthRegister('ericMa@unsw.edu.au', 'EricMa1234', 'Eric', 'Ma');
+    user1token = (user1.body as tokenReturn).token;
     // create a quiz
-    const createQuizResponse = request('POST', SERVER_URL + '/v1/admin/quiz', {
-      json: {
-        token: user1token,
-        name: 'quiz1',
-        description: 'quiz1'
-      },
-      timeout: TIMEOUT_MS
-    });
-    quiz1Id = JSON.parse(createQuizResponse.body.toString()).quizId;
-
+    const quiz1 = requestAdminQuizCreate(user1token, 'quiz1', 'quiz1');
+    quiz1Id = (quiz1.body as quizCreateResponse).quizId;
     const questionBody = {
       token: user1token,
       questionBody: {
@@ -53,100 +40,42 @@ describe('test for quiz duplicate', () => {
         ],
       },
     };
-    const createQuestionRes = request(
-      'POST',
-      SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question`,
-      {
-        json: questionBody,
-        timeout: TIMEOUT_MS
-      }
-    );
-    question1Id = JSON.parse(createQuestionRes.body.toString()).questionId;
+    const question = requestAdminQuizQuestionCreate(quiz1Id, questionBody.token, questionBody.questionBody);
+    question1Id = (question.body as quizQuestionCreateResponse).questionId;
   });
 
   test('invalid token', () => {
-    const res = request(
-      'POST',
-      SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question1Id}/duplicate`,
-      {
-        json: {
-          token: JSON.stringify('fdsafdsa')
-        },
-        timeout: TIMEOUT_MS
-      }
-    );
-    expect(res.statusCode).toStrictEqual(401);
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = requestAdminQuizQuestionDuplicate(quiz1Id, question1Id, JSON.stringify('abcd'));
+    expect(res.statusCode).toStrictEqual(httpStatus.UNAUTHORIZED);
+    expect(res.body).toStrictEqual({ error: expect.any(String) });
   });
 
   test('empty token', () => {
-    const res = request(
-      'POST',
-      SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question1Id}/duplicate`,
-      {
-        json: {
-          token: JSON.stringify('')
-        },
-        timeout: TIMEOUT_MS
-      }
-    );
-    expect(res.statusCode).toStrictEqual(401);
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = requestAdminQuizQuestionDuplicate(quiz1Id, question1Id, JSON.stringify(''));
+    expect(res.statusCode).toStrictEqual(httpStatus.UNAUTHORIZED);
+    expect(res.body).toStrictEqual({ error: expect.any(String) });
   });
 
   test('user is not an owner of this quiz', () => {
     // register user2
-    const registerResponse = request('POST', SERVER_URL + '/v1/admin/auth/register', {
-      json: {
-        email: 'EricMa@unsw.edu.au',
-        password: 'AdminPass1234',
-        nameFirst: 'Eric',
-        nameLast: 'Ma'
-      },
-      timeout: TIMEOUT_MS
-    });
-    const user2token = JSON.parse(registerResponse.body.toString()).token;
+    const user2 = requestAdminAuthRegister('EricMa@unsw.edu.au', 'ericma1234', 'Eric', 'Ma');
+    const user2token = (user2.body as tokenReturn).token;
 
-    const res = request(
-      'POST',
-      SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question1Id}/duplicate`,
-      {
-        json: {
-          token: user2token
-        },
-        timeout: TIMEOUT_MS
-      }
-    );
-    expect(res.statusCode).toStrictEqual(403);
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = requestAdminQuizQuestionDuplicate(quiz1Id, question1Id, user2token);
+    expect(res.statusCode).toStrictEqual(httpStatus.FORBIDDEN);
+    expect(res.body).toStrictEqual({ error: expect.any(String) });
   });
 
   test('quiz doesnt exists', () => {
-    const res = request(
-      'POST',
-      SERVER_URL + `/v1/admin/quiz/${quiz1Id + 1}/question/${question1Id}/duplicate`,
-      {
-        json: {
-          token: user1token
-        },
-        timeout: TIMEOUT_MS
-      }
-    );
-    expect(res.statusCode).toStrictEqual(403);
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = requestAdminQuizQuestionDuplicate(quiz1Id + 1, question1Id, user1token);
+    expect(res.statusCode).toStrictEqual(httpStatus.FORBIDDEN);
+    expect(res.body).toStrictEqual({ error: expect.any(String) });
   });
 
   test('question id doesnt refer to a valid question within the quiz', () => {
     // create quiz 2
-    const createQuizResponse = request('POST', SERVER_URL + '/v1/admin/quiz', {
-      json: {
-        token: user1token,
-        name: 'Test Quiz',
-        description: 'This is a test quiz'
-      },
-      timeout: TIMEOUT_MS
-    });
-    const quiz2Id = JSON.parse(createQuizResponse.body.toString()).quizId;
+    const quiz2 = requestAdminQuizCreate(user1token, 'quiz2', 'this is quiz2');
+    const quiz2Id = (quiz2.body as quizCreateResponse).quizId;
     // create question under quiz2
     const questionBody = {
       token: user1token,
@@ -166,62 +95,27 @@ describe('test for quiz duplicate', () => {
         ],
       },
     };
-    const createQuestionRes = request(
-      'POST',
-      SERVER_URL + `/v1/admin/quiz/${quiz2Id}`,
-      {
-        json: questionBody,
-        timeout: TIMEOUT_MS
-      }
-    );
-    const question2Id = JSON.parse(createQuestionRes.body.toString()).questionId;
+    const question2 = requestAdminQuizQuestionCreate(quiz2Id, questionBody.token, questionBody.questionBody);
+    const question2Id = (question2.body as quizQuestionCreateResponse).questionId;
     // duplicate
-    const res = request(
-      'POST',
-      SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question2Id}/duplicate`,
-      {
-        json: {
-          token: user1token
-        },
-        timeout: TIMEOUT_MS
-      }
-    );
-    expect(res.statusCode).toStrictEqual(400);
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = requestAdminQuizQuestionDuplicate(quiz1Id, question2Id, user1token);
+    expect(res.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
+    expect(res.body).toStrictEqual({ error: expect.any(String) });
   });
 
   describe('successfully duplicate a question', () => {
     test('success when only one question is in question list', () => {
       // duplicate
-      const res = request(
-        'POST',
-        SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question1Id}/duplicate`,
-        {
-          json: {
-            token: user1token
-          },
-          timeout: TIMEOUT_MS
-        }
-      );
-      expect(res.statusCode).toStrictEqual(200);
-      expect(JSON.parse(res.body.toString())).toStrictEqual(
+      const res = requestAdminQuizQuestionDuplicate(quiz1Id, question1Id, user1token);
+      expect(res.statusCode).toStrictEqual(httpStatus.SUCCESSFUL_REQUEST);
+      expect(res.body).toStrictEqual(
         { duplicatedquestionId: expect.any(Number) }
       );
-      const question2Id = JSON.parse(res.body.toString()).duplicatedquestionId;
+      const question2Id = res.body.duplicatedquestionId;
 
       // list the info in the quiz, should be two exactly same question in the list
-      const quizInfoRes = request(
-        'GET',
-        SERVER_URL + `/v1/admin/quiz/${quiz1Id}`,
-        {
-          qs: {
-            token: user1token
-          },
-          timeout: TIMEOUT_MS
-        }
-      );
-      const quizInfo = JSON.parse(quizInfoRes.body.toString());
-      expect(quizInfo).toStrictEqual({
+      const quizInfoRes = requestAdminQuizInfo(quiz1Id, user1token);
+      expect(quizInfoRes.body).toStrictEqual({
         quizId: quiz1Id,
         name: 'quiz1',
         timeCreated: expect.any(Number),
@@ -294,46 +188,20 @@ describe('test for quiz duplicate', () => {
           ],
         },
       };
-      const createQuestionRes = request(
-        'POST',
-        SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question`,
-        {
-          json: questionBody,
-          timeout: TIMEOUT_MS
-        }
-      );
-      const question2Id = JSON.parse(createQuestionRes.body.toString()).questionId;
+      const question2 = requestAdminQuizQuestionCreate(quiz1Id, user1token, questionBody.questionBody);
+      const question2Id = (question2.body as quizQuestionCreateResponse).questionId;
       // duplicate question1
-      const res = request(
-        'POST',
-        SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question1Id}/duplicate`,
-        {
-          json: {
-            token: user1token
-          },
-          timeout: TIMEOUT_MS
-        }
-      );
-      const duplicateId = JSON.parse(res.body.toString()).duplicatedquestionId;
-      expect(res.statusCode).toStrictEqual(200);
-      expect(JSON.parse(res.body.toString())).toStrictEqual(
+      const res = requestAdminQuizQuestionDuplicate(quiz1Id, question1Id, user1token);
+      const duplicateId = res.body.duplicatedquestionId;
+
+      expect(res.statusCode).toStrictEqual(httpStatus.SUCCESSFUL_REQUEST);
+      expect(res.body).toStrictEqual(
         { duplicatedquestionId: expect.any(Number) }
       );
 
       // list the info in the quiz, should be 3 questions in the list
-      const quizInfoRes = request(
-        'GET',
-        SERVER_URL + `/v1/admin/quiz/${quiz1Id}`,
-        {
-          qs: {
-            token: user1token
-          },
-          timeout: TIMEOUT_MS
-        }
-      );
-
-      const quizInfo = JSON.parse(quizInfoRes.body.toString());
-      expect(quizInfo).toStrictEqual({
+      const quizInfoRes = requestAdminQuizInfo(quiz1Id, user1token);
+      expect(quizInfoRes.body).toStrictEqual({
         quizId: quiz1Id,
         name: 'quiz1',
         timeCreated: expect.any(Number),
