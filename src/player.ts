8a70ee5817cc4c97ccfee595c2806db75ef3308c
Guanlin Kong <z5557617@ad.unsw.edu.au>
2024-11-06 12:11:11 +1100

    move function joinplayer to a new file called player.ts

---
+++ src/player.ts
@@ -0,0 +1,69 @@
+import { getData, setData } from './dataStore';
+import {
+  isStringValid,
+  generateRandomName
+} from './helperFunctions';
+
+import {
+  errorMessages,
+  playerId,
+  quizSession
+} from './interface';
+
+export enum quizState {
+  LOBBY,
+  QUESTION_COUNTDOWN,
+  QUESTION_OPEN,
+  ANSWER_SHOW,
+  FINAL_RESULTS,
+  END,
+}
+
+/**
+ *
+Allow a guest player to join a session
+ *
+ * @param {number} sessionId - sessionId of the session
+ * @param {string} playerName - the name of player
+ *
+ * @returns {errorMessages} - An object containing an error message if registration fails
+ * @returns {playerId} - A Number which is the playerId of player
+ */
+export const joinPlayer = (sessionId: number, playerName: string): errorMessages | playerId => {
+  const data = getData();
+
+  const existingPlayer = data.players.find((player) => player.playerName === playerName);
+  if (existingPlayer) {
+    throw new Error('EXIST_PLAYERNAME');
+  }
+
+  if (playerName.trim() === '') {
+    playerName = generateRandomName();
+  } else if (!isStringValid(playerName)) {
+    throw new Error('INVALID_PLAYERNAME');
+  }
+
+  let FindSession: quizSession;
+  for (const quiz of data.quizzes) {
+    FindSession = quiz.activeSessions.find((session) => session.sessionId === sessionId)
+    if (FindSession) break;
+  }
+
+  if (!FindSession) {
+    throw new Error('INVALID_SESSIONID');
+  }
+  if (FindSession.sessionState !== quizState.LOBBY) {
+    throw new Error('SESSION_NOT_IN_LOBBY');
+  }
+
+  const playerId = data.players.length + 1;
+  const newPlayer = {
+    playerId,
+    playerName,
+    sessionId,
+  };
+
+  data.players.push(newPlayer);
+  setData(data);
+  return { playerId: playerId };
+};
