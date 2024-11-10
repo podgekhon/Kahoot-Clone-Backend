b0f8319b0e2d470d9f5a3acc5eaeb50f6ff0985b
Patrick Tang Truong <z5549057@ad.unsw.edu.au>
2024-11-10 19:39:34 +1100

    test: add tests for adminGetFinalResults

---
+++ tests/adminGetFinalResults.test.ts
@@ -0,0 +1,206 @@
+import {
+  requestAdminAuthRegister,
+  requestAdminQuizCreate,
+  requestAdminQuizQuestionCreateV2,
+  requestAdminStartQuizSession,
+  httpStatus,
+  requestAdminQuizSessionUpdate,
+  requestadminQuizSessionState,
+  requestClear
+} from '../src/requestHelperFunctions';
+import {
+  userAuthRegister,
+  tokenReturn,
+  quizCreate,
+  quizCreateResponse,
+  questionCreate,
+  startSession,
+  quizStartSessionResponse,
+  quizSessionStatusUpdate,
+  sessionState,
+} from '../src/interface';
+import { adminAction } from '../src/quiz';
+// import sleepSync from 'slync';
+
+describe('Test for adminGetFinalResults', () => {
+  let user1Response: userAuthRegister;
+  let user1Token: string;
+  let quizCreateResponse: quizCreate;
+  let quizId: number;
+  let quizQuestionCreateResponse: questionCreate;
+  let startSessionResponse: startSession;
+  let sessionId: number;
+  const endAction: adminAction = adminAction.END;
+  const nextQuestionAction: adminAction = adminAction.NEXT_QUESTION;
+  const skipCountDownAction: adminAction = adminAction.SKIP_COUNTDOWN;
+  const showAnswerAction: adminAction = adminAction.GO_TO_ANSWER;
+  const goFinalResults: adminAction = adminAction.GO_TO_FINAL_RESULT;
+  let adminQuizSessionUpdate: quizSessionStatusUpdate;
+
+  beforeEach(() => {
+    requestClear();
+    user1Response = requestAdminAuthRegister(
+      'user1@gmail.com',
+      'validPassword1',
+      'User',
+      'One'
+    );
+
+    user1Token = (user1Response.body as tokenReturn).token;
+    expect(user1Response.statusCode).toStrictEqual(200);
+
+    quizCreateResponse = requestAdminQuizCreate(
+      user1Token,
+      'validQuizName',
+      'validQuizDescription'
+    );
+    expect(quizCreateResponse.statusCode).toStrictEqual(200);
+    quizId = (quizCreateResponse.body as quizCreateResponse).quizId;
+
+    const questionBody = {
+      question: 'What is the capital of Australia?',
+      timeLimit: 4,
+      points: 5,
+      answerOptions: [
+        { answer: 'Canberra', correct: true },
+        { answer: 'Sydney', correct: false },
+      ],
+      thumbnailUrl: 'http://google.com/some/image/path.jpg'
+    };
+    quizQuestionCreateResponse = requestAdminQuizQuestionCreateV2(
+      quizId,
+      user1Token,
+      questionBody
+    );
+    expect(quizQuestionCreateResponse.statusCode).toStrictEqual(
+      httpStatus.SUCCESSFUL_REQUEST
+    );
+
+    startSessionResponse = requestAdminStartQuizSession(
+      quizId,
+      user1Token,
+      1
+    );
+    expect(startSessionResponse.statusCode).toStrictEqual(
+      httpStatus.SUCCESSFUL_REQUEST
+    );
+
+    sessionId = (
+      startSessionResponse.body as quizStartSessionResponse
+    ).sessionId;
+
+    const resStartSession = requestadminQuizSessionState(
+      quizId,
+      sessionId,
+      user1Token
+    );
+    const sessionState = resStartSession.body.state;
+    expect(sessionState).toStrictEqual('LOBBY');
+  });
+
+  test('User successfully gets final results', () => {
+    // player join session
+
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, nextQuestionAction);
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, skipCountDownAction);
+
+    // players answer question
+
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, goFinalResults);
+
+    adminGetFinalResults = requestAdminGetFinalResults(
+      quizId,
+      sessionId,
+      user1Token
+    );
+
+    expect(adminGetFinalResults.statusCode).toStrictEqual(
+      httpStatus.SUCCESSFUL_REQUEST
+    );
+    expect(adminGetFinalResults.body).toStrictEqual({
+      usersRankedByScore: [
+        {
+          playerName: 'player1',
+          score: expect.any(Number)
+        }
+      ],
+      questionResults: [
+        {
+          questionId: expect.any(Number),
+          plaeyersCorrect: [
+            'player1'
+          ],
+          averageAnswerTime: expect.any(Number),
+          percentCorrect: expect.any(Number)
+        }
+      ]
+    });
+  });
+
+  test('Return error for invalid SessionId', () => {
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, nextQuestionAction);
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, skipCountDownAction);
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, showAnswerAction);
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, goFinalResults);
+
+    adminGetFinalResults = requestAdminGetFinalResults(
+      quizId,
+      sessionId + 1,
+      user1Token
+    );
+
+    expect(adminGetFinalResults.statusCode).toStrictEqual(
+      httpStatus.BAD_REQUEST
+    );
+    expect(adminGetFinalResults.body).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('Return error for session not in FINAL_RESULTS state', () => {
+    adminGetFinalResults = requestAdminGetFinalResults(
+      quizId,
+      sessionId,
+      user1Token
+    );
+
+    expect(adminGetFinalResults.statusCode).toStrictEqual(
+      httpStatus.BAD_REQUEST
+    );
+    expect(adminGetFinalResults.body).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('Return error for invalid token', () => {
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, nextQuestionAction);
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, skipCountDownAction);
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, showAnswerAction);
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, goFinalResults);
+
+    adminGetFinalResults = requestAdminGetFinalResults(
+      quizId,
+      sessionId,
+      user1Token + 1
+    );
+
+    expect(adminGetFinalResults.statusCode).toStrictEqual(
+      httpStatus.UNAUTHORIZED
+    );
+    expect(adminGetFinalResults.body).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('Return error for invalid quizId', () => {
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, nextQuestionAction);
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, skipCountDownAction);
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, showAnswerAction);
+    requestAdminQuizSessionUpdate(quizId, sessionId, user1Token, goFinalResults);
+
+    adminGetFinalResults = requestAdminGetFinalResults(
+      quizId + 1,
+      sessionId,
+      user1Token
+    );
+
+    expect(adminGetFinalResults.statusCode).toStrictEqual(
+      httpStatus.FORBIDDEN
+    );
+    expect(adminGetFinalResults.body).toStrictEqual({ error: expect.any(String) });
+  });
+});
