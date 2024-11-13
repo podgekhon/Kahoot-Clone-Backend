f8a055e397a4c4b56a8554d09a5bbf4483f6f234
Xiaoyuan Ma <z5538765@ad.unsw.edu.au>
2024-11-13 23:23:57 +1100

    perf: improve testing quality, save more time

--- tests/adminQuizSessionUpdate.test.ts
+++ tests/adminQuizSessionUpdate.test.ts
@@ -188,7 +188,10 @@ describe('Test for adminQuizSessionUpdate', () => {
       nextQuestionAction
     );
 
-    sleepSync(65 * 1000);
+    sleepSync(3 * 1000);
+
+
+    sleepSync(5 * 1000);
 
     getUpdatedSession = requestadminQuizSessionState(
       quizId,
