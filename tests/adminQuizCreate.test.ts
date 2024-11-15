98bb3246b62b41a7a628dad3ac6e9e75a1ff3975
Xiaoyuan Ma <z5538765@ad.unsw.edu.au>
2024-11-15 13:39:09 +1100

    style: using for each for testing v1 and v2 routes

--- tests/adminQuizCreate.test.ts
+++ tests/adminQuizCreate.test.ts
@@ -7,7 +7,6 @@ import {
 } from '../src/requestHelperFunctions';
 
 import { tokenReturn, quizCreateResponse, userAuthRegister } from '../src/interface';
-import { adminQuizCreate } from '../src/quiz';
 
 export enum httpStatus {
   UNAUTHORIZED = 401,
@@ -30,84 +29,83 @@ describe('adminQuizCreate', () => {
     {
       version: 'v1',
       quizCreate: requestAdminQuizCreate,
-    },{
+    }, {
       version: 'v2',
       quizCreate: requestAdminQuizCreateV2
     }
-  ]
-  testCase.forEach(({version, quizCreate}) => {
-
+  ];
+  testCase.forEach(({ version, quizCreate }) => {
     describe(`Test for ${version}`, () => {
       test('invalid token', () => {
         const result = quizCreate('hahainvalid', 'Quiz1', 'Invalid token');
-        
+
         expect(result.statusCode).toStrictEqual(httpStatus.UNAUTHORIZED);
         expect(result.body).toStrictEqual({ error: expect.any(String) });
       });
-      
+
       test('empty token', () => {
         const result = quizCreate('', 'Quiz1', 'Empty token');
-        
+
         expect(result.statusCode).toStrictEqual(httpStatus.UNAUTHORIZED);
         expect(result.body).toStrictEqual({ error: expect.any(String) });
       });
-      
+
       test('name less than 3 characters', () => {
         const result = quizCreate(user1Token, '1', 'Too short name');
-        
+
         expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
         expect(result.body).toStrictEqual({ error: expect.any(String) });
       });
-      
+
       // Name exceeds 30 characters
       test('name more than 30 characters', () => {
         const longName = 'a'.repeat(31);
-        const result =  quizCreate(user1Token, longName, 'Name too long');
-        
+        const result = quizCreate(user1Token, longName, 'Name too long');
+
         expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
         expect(result.body).toStrictEqual({ error: expect.any(String) });
       });
-      
+
       test('name contains invalid characters', () => {
         const result = quizCreate(user1Token, '~invalidname', 'Invalid characters');
-        
+
         expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
         expect(result.body).toStrictEqual({ error: expect.any(String) });
       });
-      
+
       // Description exceeds 100 characters
       test('description is more than 100 characters', () => {
         const longDescription = 'a'.repeat(101);
         const result = quizCreate(user1Token, 'quiz1', longDescription);
-        
+
         expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
         expect(result.body).toStrictEqual({ error: expect.any(String) });
       });
-      
+
       test('duplicate quiz names owned by same user', () => {
         requestAdminQuizCreate(user1Token, 'quiz1', 'First quiz');
-        
+
         const result = quizCreate(user1Token, 'quiz1', 'Second quiz with same name');
-        
+
         expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
         expect(result.body).toStrictEqual({ error: expect.any(String) });
       });
-      
+
       // Valid input test
       test('valid inputs', () => {
         const result = quizCreate(user1Token, 'quiz1', 'This is quiz 1');
-        
+
         expect(result.statusCode).toStrictEqual(httpStatus.SUCCESSFUL_REQUEST);
         expect(result.body).toStrictEqual({ quizId: expect.any(Number) });
-        
+
         // Validate quiz is in the quiz list
         const quizList = requestAdminQuizList(user1Token);
         const quizId = (result.body as quizCreateResponse).quizId;
-        
+
         expect(quizList.body).toStrictEqual({
           quizzes: [{ quizId: quizId, name: 'quiz1' }]
         });
       });
     });
-  })
+  });
 });
--- tests/adminUserPasswordUpdate.test.ts
+++ tests/adminUserPasswordUpdate.test.ts
@@ -16,123 +16,95 @@ const invalidPasswords = [
   { password: '' },
 ];
 
-beforeEach(() => {
-  requestClear();
-});
-
 /// //////-----adminUserPasswordUpdate-----//////////
 describe('test for adminUserPasswordUpdate', () => {
   let user1;
-  let user1token: string;
+  let user1Token: string;
 
   beforeEach(() => {
+    requestClear();
     user1 = requestAdminAuthRegister('ericMa@unsw.edu.au', 'EricMa1234', 'Eric', 'Ma');
-    user1token = (user1.body as tokenReturn).token;
+    user1Token = (user1.body as tokenReturn).token;
   });
 
-  // authUserId is not valid user
-  test('invalid token', () => {
-    const result = requestAdminUserPasswordUpdate(
-      JSON.stringify('abcd'),
-      'EricMa1234',
-      '1234EricMa');
-    expect(result.statusCode).toStrictEqual(httpStatus.UNAUTHORIZED);
-    expect(result.body).toStrictEqual({ error: expect.any(String) });
-  });
+  const testCase = [
+    {
+      version: 'v1',
+      passwordUpdate: requestAdminUserPasswordUpdate,
+    }, {
+      version: 'v2',
+      passwordUpdate: requestAdminUserPasswordUpdateV2
+    }
+  ];
 
-  test('empty token', () => {
-    const result = requestAdminUserPasswordUpdate(JSON.stringify(''), 'EricMa1234', '1234EricMa');
-    expect(result.statusCode).toStrictEqual(httpStatus.UNAUTHORIZED);
-    expect(result.body).toStrictEqual({ error: expect.any(String) });
-  });
+  testCase.forEach(({ version, passwordUpdate }) => {
+    describe(`Test for ${version}`, () => {
+      // authUserId is not valid user
+      test('invalid token', () => {
+        const result = passwordUpdate(
+          JSON.stringify('abcd'),
+          'EricMa1234',
+          '1234EricMa');
+        expect(result.statusCode).toStrictEqual(httpStatus.UNAUTHORIZED);
+        expect(result.body).toStrictEqual({ error: expect.any(String) });
+      });
 
-  // Old password is not the correct old password
-  test('old password is wrong', () => {
-    const result = requestAdminUserPasswordUpdate(user1token, 'wrongpassword', '1234EricMa');
-    expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
-    expect(result.body).toStrictEqual({ error: expect.any(String) });
-  });
+      test('empty token', () => {
+        const result = passwordUpdate(JSON.stringify(''), 'EricMa1234', '1234EricMa');
+        expect(result.statusCode).toStrictEqual(httpStatus.UNAUTHORIZED);
+        expect(result.body).toStrictEqual({ error: expect.any(String) });
+      });
 
-  // Old password and new password match exactly
-  test('new password is same as the old one', () => {
-    const result = requestAdminUserPasswordUpdate(user1token, 'EricMa1234', 'EricMa1234');
-    expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
-    expect(result.body).toStrictEqual({ error: expect.any(String) });
-  });
+      // Old password is not the correct old password
+      test('old password is wrong', () => {
+        const result = passwordUpdate(user1Token, 'wrongpassword', '1234EricMa');
+        expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
+        expect(result.body).toStrictEqual({ error: expect.any(String) });
+      });
 
-  // New password has already been used before by this user
-  test('new password has been used before', () => {
-    requestAdminUserPasswordUpdate(user1token, 'EricMa1234', '1234EricMa');
-    // update again
-    const result = requestAdminUserPasswordUpdate(user1token, '1234EricMa', 'EricMa1234');
-    expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
-    expect(result.body).toStrictEqual({ error: expect.any(String) });
-  });
+      // Old password and new password match exactly
+      test('new password is same as the old one', () => {
+        const result = passwordUpdate(user1Token, 'EricMa1234', 'EricMa1234');
+        expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
+        expect(result.body).toStrictEqual({ error: expect.any(String) });
+      });
 
-  // test for invalid passwords(too short, no characters/numbers)
-  test('invalid new passwords', () => {
-    invalidPasswords.forEach(({ password }) => {
-      const result = requestAdminUserPasswordUpdate(user1token, 'EricMa1234', password);
-      expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
-      expect(result.body).toStrictEqual({ error: expect.any(String) });
-    });
-  });
+      // New password has already been used before by this user
+      test('new password has been used before', () => {
+        passwordUpdate(user1Token, 'EricMa1234', '1234EricMa');
+        // update again
+        const result = passwordUpdate(user1Token, '1234EricMa', 'EricMa1234');
+        expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
+        expect(result.body).toStrictEqual({ error: expect.any(String) });
+      });
 
-  // correct return type
-  test('Correct return type', () => {
-    const result = requestAdminUserPasswordUpdate(user1token, 'EricMa1234', '1234EricMa');
-    expect(result.statusCode).toStrictEqual(httpStatus.SUCCESSFUL_REQUEST);
-    expect(result.body).toStrictEqual({});
+      // test for invalid passwords(too short, no characters/numbers)
+      test('invalid new passwords', () => {
+        invalidPasswords.forEach(({ password }) => {
+          const result = passwordUpdate(user1Token, 'EricMa1234', password);
+          expect(result.statusCode).toStrictEqual(httpStatus.BAD_REQUEST);
+          expect(result.body).toStrictEqual({ error: expect.any(String) });
+        });
+      });
 
-    // try to login by using the old password
-    // should fail
-    let res = requestAdminAuthLogin('ericMa@unsw.edu.au', 'EricMa1234');
-    expect(res.statusCode).toStrictEqual(400);
-    expect(res.body).toStrictEqual({ error: expect.any(String) });
+      // correct return type
+      test('Correct return type', () => {
+        const result = passwordUpdate(user1Token, 'EricMa1234', '1234EricMa');
+        expect(result.statusCode).toStrictEqual(httpStatus.SUCCESSFUL_REQUEST);
+        expect(result.body).toStrictEqual({});
 
-    // try to login with the new password
-    // should success
-    res = requestAdminAuthLogin('ericMa@unsw.edu.au', '1234EricMa');
-    expect(res.statusCode).toStrictEqual(200);
-    expect(res.body).toStrictEqual({ token: expect.any(String) });
-  });
-  describe('tests for v2 routes', () => {
-    test('success case', () => {
-      // update the password, using v2 route
-      const result = requestAdminUserPasswordUpdateV2(user1token, 'EricMa1234', '1234EricMa');
-      expect(result.statusCode).toStrictEqual(httpStatus.SUCCESSFUL_REQUEST);
-      expect(result.body).toStrictEqual({});
+        // try to login by using the old password
+        // should fail
+        let res = requestAdminAuthLogin('ericMa@unsw.edu.au', 'EricMa1234');
+        expect(res.statusCode).toStrictEqual(400);
+        expect(res.body).toStrictEqual({ error: expect.any(String) });
 
-      // try to login by using the old password
-      // should fail
-      let res = requestAdminAuthLogin('ericMa@unsw.edu.au', 'EricMa1234');
-      expect(res.statusCode).toStrictEqual(400);
-      expect(res.body).toStrictEqual({ error: expect.any(String) });
-
-      // try to login with the new password
-      // should success
-      res = requestAdminAuthLogin('ericMa@unsw.edu.au', '1234EricMa');
-      expect(res.statusCode).toStrictEqual(200);
-      expect(res.body).toStrictEqual({ token: expect.any(String) });
-    });
-
-    // authUserId is not valid user
-    test('invalid token', () => {
-      const result = requestAdminUserPasswordUpdateV2(
-        JSON.stringify('abcd'),
-        'EricMa1234',
-        '1234EricMa');
-      expect(result.statusCode).toStrictEqual(httpStatus.UNAUTHORIZED);
-      expect(result.body).toStrictEqual({ error: expect.any(String) });
-    });
-
-    test('empty token', () => {
-      const result = requestAdminUserPasswordUpdateV2(
-        JSON.stringify(''),
-        'EricMa1234',
-        '1234EricMa');
-      expect(result.statusCode).toStrictEqual(httpStatus.UNAUTHORIZED);
-      expect(result.body).toStrictEqual({ error: expect.any(String) });
+        // try to login with the new password
+        // should success
+        res = requestAdminAuthLogin('ericMa@unsw.edu.au', '1234EricMa');
+        expect(res.statusCode).toStrictEqual(200);
+        expect(res.body).toStrictEqual({ token: expect.any(String) });
+      });
     });
   });
 });
